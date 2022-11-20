a76871f5a8ef1fb75561f3d0450ce8f4e21048f4
Amritgrg1 <gamrit450@gmail.com>
2022-11-20 13:30:19 +0545

    login_form

---
+++ main.js
@@ -0,0 +1,33 @@
+/*=============== SHOW HIDDEN - PASSWORD ===============*/
+
+const showHiddenPass = (inputPass,inputIcon) =>{
+    const input = document.getElementById(inputPass),
+    iconEye = document.getElementById(inputIcon)
+
+    iconEye.addEventListener('click', () =>{
+        // Change password to text
+        if(input.type === 'password')
+        {
+            //switch to text
+            input.type = 'text'
+
+            //Add icon
+            iconEye.classList.add('ri-eye-line')
+
+            // Remove icon
+            iconEye.classList.remove('ri-eye-off-line')
+        }
+        else{
+            //change to password
+            input.type = 'password'
+
+            // Add icon
+            iconEye.classList.add('ri-eye-off-line')
+
+            //Remove icon
+            iconEye.classList.remove('ri-eye-line')
+        }
+    })
+}
+
+showHiddenPass('input-pass','input-icon')
